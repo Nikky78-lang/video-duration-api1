167f958ff87e6183f5161dcb1d7b9e5e1bc3def0
Nikky78-lang <v.lazarenko@futurragroup.com>
2025-04-23 22:41:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import requests  # <-- Додано цей імпорт
 import moviepy.editor as mp
 import os
 import tempfile
+from urllib.parse import unquote
 
 app = Flask(__name__)
 
 @app.route('/duration')
 def get_duration():
-    url = request.args.get('url')
+    url = unquote(request.args.get('url'))
     if not url:
         return jsonify({'error': 'Missing URL parameter'}), 400
     try:
         with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_file:
             temp_file.write(requests.get(url).content)
-            #video = mp.VideoFileClip(temp_file.name)
+            video = mp.VideoFileClip(temp_file.name)
             os.remove(temp_file_path)
-            return jsonify(requests.get(url).content)
+            return jsonify(video.duration)
         return jsonify(0)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
